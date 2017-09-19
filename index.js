5a81180e5c9fc7806285292f32ad4478b80ddd0e
Jonathan Vargas <jonathanv3232@gmail.com>
2017-09-19 14:31:01 -0400

    adds second hand functionality

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const secondHand = document.querySelector('.second-hand');
 function setDate() {
-  const = new Date();
-  
+  console.log("hi");
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const mins = now.getMinutes();
+  const hours = now.getHours();
+  const secondDegrees = (seconds / 60 * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
 }
 
 setInterval(setDate, 1000);
